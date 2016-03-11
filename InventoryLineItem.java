40abc5b8e7743c2ae9031824a86e6e2dfbfc4404
David Punzel <dpunzel15@georgefox.edu>
2016-03-11 14:47:12 -0800

    updated setLineItemSubtotal

--- InventoryLineItem.java
+++ InventoryLineItem.java
@@ -3,7 +3,7 @@
  */
 public class InventoryLineItem
 {
-    private static int instanceCounter;
+    private static int _instanceCounter;
     private int _lineItemNum;
     private Product _product;
     private int _lineItemQty;
@@ -13,10 +13,12 @@ public class InventoryLineItem
 
     public InventoryLineItem(Product product, int lineItemQty)
     {
-        InventoryLineItem.instanceCounter++;
-        _lineItemNum = instanceCounter;
+        InventoryLineItem._instanceCounter++;
+        _lineItemNum = _instanceCounter;
         this._product = product;
         this._lineItemQty = lineItemQty;
+        this.setLineItemDiscountRate();
+        this.setLineItemSubtotal();
     }
 
     public void setLineItemDiscountRate()
@@ -43,7 +45,7 @@ public class InventoryLineItem
 
     public void setLineItemSubtotal()
     {
-        _lineItemSubtotal = _lineItemQty * Product.getProductPrice() * _lineItemDiscountRate;
+        _lineItemSubtotal = _lineItemQty * getProduct().getProductPrice() * _lineItemDiscountRate;
     }
 
     public int getLineItemNum()
@@ -86,9 +88,9 @@ public class InventoryLineItem
     public static void main(String[] args)
     {
         CD myCD = new CD("id", "Classical", 24.99, "mozart");
-        Book myBook = new Book("theId", "aBook", 52.99, "BookTitleWow", "AwesomeAuthor");
-        InventoryLineItem myILI = new InventoryLineItem(myCD, 25);
-        InventoryLineItem myILI2 = new InventoryLineItem(myBook, 50);
+        //Book myBook = new Book("theId", "aBook", 52.99, "BookTitleWow", "AwesomeAuthor");
+        InventoryLineItem myILI = new InventoryLineItem(myCD, 49);
+        //InventoryLineItem myILI2 = new InventoryLineItem(myBook, 50);
         //CD myCD2 = new CD("id", "Classical2", 29.99, "mozart2");
         System.out.println(myCD.getProductDesc());
         System.out.println(myCD.getProductPrice());
@@ -98,11 +100,12 @@ public class InventoryLineItem
         //InventoryLineItem myILI2 = new InventoryLineItem(myCD2, 250);
         myILI.setLineItemSubtotal();
         myILI.setLineItemDiscountRate();
-        myILI2.setLineItemDiscountRate();
+        //myILI2.setLineItemDiscountRate();
         System.out.println(myILI.getLineItemDiscountRate());
         System.out.println(myILI.toString());
-        System.out.println(myILI2.toString());
+        //System.out.println(myILI2.toString());
         System.out.println(myILI.getLineItemSubtotal());
+        System.out.println(myILI.getProduct().getProductPrice());
 
 
 
